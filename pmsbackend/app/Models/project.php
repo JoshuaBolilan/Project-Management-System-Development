64ffb0f6ca895d84bb883999488d4faa786a0ceb
Clarisse Cartagena <clarissepcartagena@gmail.com>
2025-05-20 21:21:57 +0800

    update project.php

--- pmsbackend/app/Models/project.php
+++ pmsbackend/app/Models/project.php
@@ -6,16 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    // Specifies the attributes that can be mass-assigned
     protected $fillable = [
-        'name',
-        'description',
-        'start_date',
-        'end_date',
-        'user_id',
-        'status',
-        'budget',
+        'name',         // Name of the project
+        'description',  // Description of the project
+        'start_date',   // Start date of the project
+        'end_date',     // End date of the project
+        'user_id',      // ID of the user who created or manages the project
+        'status',       // Current status of the project (e.g., active, completed)
+        'budget',       // Budget allocated for the project
     ];
-    
-
-    
 }
